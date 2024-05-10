6cbd626fb263997030efb11d35f88c8c61d8895f
Emmanuel Bajan <emmanuelbajan0@gmail.com>
2024-05-10 10:22:47 +0800

    get current address

--- AttendanceDetails.js
+++ AttendanceDetails.js
@@ -6,10 +6,22 @@ const AttendanceDetailSchema = new mongoose.Schema({
     date: String,
     time_in: String,
     time_out: String,
-    time_in_latitude: String,
-    time_in_longitude: String,
-    time_out_latitude: String,
-    time_out_longitude: String
+    time_in_latitude: 
+     {
+        type: Number
+     },
+    time_in_longitude: 
+     {
+        type: Number
+     },
+    time_out_latitude: 
+     {
+        type: Number
+     },
+    time_out_longitude:
+     {
+        type: Number
+     }
 },{
     collection:"attendances"
 });
